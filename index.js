64374beab6dcdfd540a769f9103101b766cbf9a9
Dave Kidd <dave@davekidd.net>
2021-12-08 19:20:02 -0700

    monolith

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const dotenv = require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const path = require("path")
 const port = process.env.PORT || 9000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname,"client/build")))
 
-app.use("/api/",(_,res)=>{
+app.use("/api/*",(_,res)=>{
     res.json({data:"API Data Served!"})
 })
 
@@ -15,6 +17,9 @@ app.listen(port,()=>{
     console.log(`Server is on port ${port}`)
 })
 
+app.use("*",(_,res)=>{
+    res.sendFile(path.join(__dirname,"client/build","index.html"))
+})
 
 // apiKey = 93848903844 BAD DON'T DO THIS
 // dbpw = "abc123" USE ENV VARS!
